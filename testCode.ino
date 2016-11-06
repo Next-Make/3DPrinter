f8a1b8f0ecba4525a7fb93a816ef77d6a02234a7
milesdai <milesdai16@gmail.com>
2016-11-06 01:22:24 -0400

    Add files via upload

---
+++ testCode.ino
@@ -0,0 +1,17 @@
+#include <RepStepper.h>
+
+RepStepper *myStepper;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  myStepper = new RepStepper(50,4,3,5);
+  myStepper->setRPM(35);
+  myStepper->enable();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  myStepper->pulse();
+  delay(.5);
+}
